15c58c909fa4f428e9e8a7bf42998b6d80c597e4
Thomas Stassin <thomas.stassin@gsmproductions.com>
2022-05-10 18:48:09 +0200

    add jump

--- game.py
+++ game.py
@@ -5,20 +5,22 @@ WIDTH = 800
 HEIGHT = 600
 
 GROUND = 458
+GRAVITY = 100
 
 N_BG = 2
 
 hero = Actor("hero", anchor=('middle', 'bottom'))
 hero.pos = (64, GROUND)
+hero_speed = 0
 
-speed = 100
+game_speed = 100
 
 backgrounds_bottom = []
 backgrounds_top = []
 
 for n in range(N_BG):
     bg_b = Actor("bg_1", anchor=('left', 'top'))
-    bg_b.pos = n * WIDTH, 0  
+    bg_b.pos = n * WIDTH, 0
     backgrounds_bottom.append(bg_b)
 
     bg_t = Actor("bg_2", anchor=('left', 'top'))
@@ -33,15 +35,30 @@ def draw():
         bg.draw()
 
     for bg in backgrounds_top:
-        bg.draw();
+        bg.draw()
     hero.draw()
 
 
 def update(dt):
 
+    # hero update
+    global hero_speed
+
+    hero_speed -= GRAVITY * dt
+    x, y = hero.pos
+    y -= hero_speed * dt
+
+    if y > GROUND:
+        y = GROUND
+        hero_speed = 0
+
+    hero.pos = x, y
+
+    # bg update
+
     for bg in backgrounds_bottom:
         x, y = bg.pos
-        x = x - speed * dt
+        x -= game_speed * dt
         bg.pos = x, y
 
     if backgrounds_bottom[0].pos[0] < - WIDTH:
@@ -51,10 +68,19 @@ def update(dt):
 
     for bg in backgrounds_top:
         x, y = bg.pos
-        x = x - speed/3 * dt
+        x -= game_speed/3 * dt
         bg.pos = x, y
 
     if backgrounds_top[0].pos[0] < - WIDTH:
         bg = backgrounds_top.pop(0)
         bg.pos = (N_BG - 1) * WIDTH, 0
         backgrounds_top.append(bg)
+
+
+def on_key_down(key):
+    global hero_speed
+
+    if key == keys.SPACE:
+
+        if hero_speed <= 0:
+            hero_speed = 200
